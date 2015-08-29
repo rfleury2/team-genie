4d5de55b21990d7849f8a02aacbb97d76b9cec37
Ricardo Fleury <ricardo.fleury@Centro-200556.centro.net>
2015-08-29 14:51:53 -0500

    Edit user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,33 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		find_user_by_id
+		redirect_to new_auth_path unless can_edit?
+	end
+
+	def update
+		find_user_by_id
+		if can_edit?
+			@user.attributes = user_params
+			@user.save(validate: false)
+			redirect_to root_path
+		else
+			redirect_to new_auth_path
+		end
+	end
+
 	private 
 
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
+	def can_edit?
+		@user && (@user == current_user)
+	end
+
+	def find_user_by_id
+		@user = User.find_by(id: params[:id])
 	end
 end
