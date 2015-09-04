5fec69e11f7965321d028b96d9937c4dd343a885
Ricardo Fleury <ricardo.fleury@Centro-200556.centro.net>
2015-09-04 15:07:37 -0500

    Add teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823175542) do
+ActiveRecord::Schema.define(version: 20150904194850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20150823175542) do
   add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+
+  create_table "teams", force: :cascade do |t|
+    t.string  "name"
+    t.integer "captain_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
