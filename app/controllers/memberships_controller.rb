cac8937855442ab69ed997e4c027812f4b12a9e4
Ricardo Fleury <ricardo.fleury@Centro-200556.centro.net>
2015-09-12 22:18:43 -0500

    Invite an existing user to join a team

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,6 +3,37 @@ class MembershipsController < ApplicationController
 	end
 
 	def create
-		redirect_to :back
+		find_team_by_id
+		if invited_player_is_user?(membership_params['email']) && !@team.is_member?(@player)
+			@player.memberships.create(team: @team)
+		else
+			# errors won't show until I js this
+			assign_errors
+		end
+		redirect_to team_path(@team, result: @something)
+	end
+
+	private 
+
+	def assign_errors
+		@errors = []
+		@errors << already_belongs_error if @team.is_member?(@player)
+		@errors << "Not a user - to be implemented later" if !invited_player_is_user?(membership_params['email'])
+	end
+
+	def invited_player_is_user?(email)
+		@player = User.find_by_email(email)
+	end
+
+	def already_belongs_error
+		"Player is already a team member."
+	end
+
+	def find_team_by_id
+		@team = Team.find_by(id: params[:team_id])
+	end
+
+	def membership_params
+		params.require(:membership).permit(:email)
 	end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -12,4 +12,8 @@ class Team < ActiveRecord::Base
 	# Image upload
 	has_attached_file :avatar, styles: { medium: "300x300>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  def is_member?(player)
+  	self.memberships.pluck(:player_id).include?(player.id) if player
+  end
 end
