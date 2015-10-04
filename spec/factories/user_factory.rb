ecbb5812e42e98f54005b7d0ff73bed4a1edf791
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2015-10-04 18:29:45 -0500

    Add specs around membership creation for new users

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user, class: User do
     name "John Person"
-    email  "email@example.com"
+    email  "email@example#{Random.rand(10000)}.com"
     password "password"
     password_confirmation "password"
     role "player"
