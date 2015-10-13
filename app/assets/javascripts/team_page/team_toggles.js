e714ffe73856aba82c6cb400e6281bb7de53f54d
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2015-10-13 18:12:17 -0500

    Change tabs jQuery

---
+++ app/assets/javascripts/team_page/team_toggles.js
@@ -0,0 +1,24 @@
+$(document).ready( function() {
+	$(document).on('click', '#roster', function(e) {
+		changeTabs(this)
+	});
+
+	$(document).on('click', '#games', function(e) {
+		changeTabs(this)
+	});
+
+	$(document).on('click', '#settings', function(e) {
+		changeTabs(this)
+	});
+});
+
+
+function changeTabs(selectedTab) {
+	if (!$(selectedTab).hasClass('active')) {
+		var tabs = $('.team-nav')
+		for(var i = 0; i < tabs.length; i++) {
+			$(tabs[i]).removeClass('active')
+		}
+		$(selectedTab).addClass('active')
+	}
+}
