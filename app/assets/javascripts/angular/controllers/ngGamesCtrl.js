6e38ff4b7f579f55eb05f82312d3872f06f6f0e2
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2015-10-17 14:42:34 -0500

    Implement delete game front and back end logic

--- app/assets/javascripts/angular/controllers/ngGamesCtrl.js
+++ app/assets/javascripts/angular/controllers/ngGamesCtrl.js
@@ -9,13 +9,20 @@ ngTeamGenie.controller('ngGamesCtrl', function NgGamesCtrl($scope, GamesRoutes) 
 		$scope.games.push(newlyCreatedGame)
 	}
 
-	// PUT - UPDATE
-	$scope.editGame = function() {
-		
+	// DELETE - DELETE
+	$scope.deleteGame = function(game) {
+		GamesRoutes.remove(game)
+		// TODO: Add "if successful logic"
+		$scope.updateGames(game)
 	}
 
-	// DELETE - DELETE
-	$scope.deleteGame = function() {
+	$scope.updateGames = function(removedGame) {
+		for (var i = 0; i < $scope.games.length; i++) { 
+			if($scope.games[i].id === removedGame.id) {
+				$scope.games.splice(i, 1)
+			}
+    	
+		}
 
 	}
 });
